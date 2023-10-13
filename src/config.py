4cf79753acc22be1c19d34c5ec52f1e83c6b4a47
Ronaldo Fernandes Roque <>
2023-10-13 17:22:55 -0300

    Add variáveis de ambiente e configurações iniciais.

---
+++ src/config.py
@@ -0,0 +1,39 @@
+import os
+import sys
+from functools import lru_cache
+
+import pytz
+from pydantic import BaseSettings
+
+tz = pytz.timezone('America/Sao_Paulo')
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+
+class Settings(BaseSettings):
+
+    debug: bool = False
+    secret_key: str
+    algorithm: str = "HS256"
+    allowed_host: str
+    allowed_origins: str
+
+    project_name: str = "api"
+    version: str = "0.1.0"
+    description: str = ""
+
+    log_file_pattern: str
+    log_level: str
+    log_rotation: str
+    log_retention: int
+
+    mapbox_access_token: str = ""
+
+    class Config:
+        env_file = f"{BASE_DIR}/.env"
+
+
+@lru_cache()
+def get_settings() -> Settings:
+    settings = Settings()
+    return settings
--- src/index.py
+++ src/index.py
@@ -1,20 +1,89 @@
-from fastapi import FastAPI
+import locale
+from http import HTTPStatus
+from typing import Optional
 
-from src.dtos.ISayHelloDto import ISayHelloDto
+from requests import get
+from orjson import orjson
 
-app = FastAPI()
+import uvicorn
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.datastructures import Default
+from fastapi.responses import HTMLResponse, ORJSONResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+
+from src.config import get_settings as settings
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def create_app() -> FastAPI:
+
+    app = FastAPI(
+        debug=settings().debug,
+        title=settings().project_name,
+        description=settings().description,
+        version=settings().version,
+        default_response_class=Default(ORJSONResponse)
+    )
+
+    api_version = "/api/v1"
+    app.mount("/static", StaticFiles(directory="static"), name="static")
+    return app
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+app = create_app()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 
-@app.post("/hello")
-async def hello_message(dto: ISayHelloDto):
-    return {"message": f"Hello {dto.message}"}
+class Coordenadas(BaseModel):
+    origem_longitude: float
+    origem_latitude: float
+    destino_longitude: float
+    destino_latitude: float
+    ida_e_volta: Optional[bool] = True
+
+    def __str__(self):
+        return f"{self.origem_longitude},{self.origem_latitude};{self.destino_longitude},{self.destino_latitude}"
+
+
+@app.post("/distance", response_class=HTMLResponse)
+async def distance(request: Request, coordenadas: Coordenadas):
+    endpoint_mapbox = f"https://api.mapbox.com/directions/v5/mapbox/driving"
+    params = {
+        'geometries': "geojson",
+        'access_token': settings().mapbox_access_token,
+    }
+    url = f"{endpoint_mapbox}/{coordenadas}"
+    mapbox_return = get(url=url, params=params)
+    response = orjson.loads(mapbox_return.text)
+    distance = None
+    try:
+        distance_float: float = (response['routes'][0]['distance'])/1000
+        formato_decimal = "%.{0:d}f".format(2)
+        distance = locale.format_string(formato_decimal, distance_float, grouping=True, monetary=False)
+    except TypeError:
+        raise HTTPException(status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail="Erro interno de tipo.")
+    except AttributeError:
+        raise HTTPException(status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail="Erro interno na resposta da API")
+    return templates.TemplateResponse("item.html", {"request": request, "distance": distance})
+
+
+@app.get("/items/{id}", response_class=HTMLResponse)
+async def read_item(request: Request, id: str):
+    return templates.TemplateResponse("item.html", {"request": request, "id": id})
+
+
+@app.get("/api")
+def read_root():
+    return {"Hello": "World"}
+
+
+def start():
+    """Launched with `poetry run start` at root level"""
+    uvicorn.run("index:app", host="127.0.0.1", port=3000, reload=True)
+
+
+if __name__ == "__main__":
+    start()
